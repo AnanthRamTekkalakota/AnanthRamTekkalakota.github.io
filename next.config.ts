f84f230dcc255d2263bb01281410ce05dba196e7
Razer323 <razer32323@gmail.com>
2025-05-24 04:39:11 -0500

    attempting to fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  
+  distDir: "docs",
   images: { unoptimized: true },
-  
 };
+
 export default nextConfig;
